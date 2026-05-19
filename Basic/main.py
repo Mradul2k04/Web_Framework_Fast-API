3e01f43685520fc68404667e317f73886d488ee3
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-05-19 11:24:46 +0530

    [Mradul Sharma] Update the path and query parameter in FastAPI

--- Basic/main.py
+++ Basic/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Path
+from fastapi import FastAPI , Path,HTTPException,Query
 import json
 
 app=FastAPI()
@@ -32,4 +32,20 @@ def view_patient(patient_id:str = Path(...,description="ID of patient in the DB 
     
     if patient_id in data:
         return data[patient_id]
-    return {'error':"patient not found"}
+    raise HTTPException(status_code=404,detail="Patient not found" )
+
+
+@app.get("/sort")
+def sort_patients(sort_by:str=Query(...,description="Sort on the basis on height,weight,bmi"), order:str=Query('asc',description="sort in asc or desc order")):
+    vaild_fields=['height','weight','bmi']
+    
+    if sort_by not in vaild_fields:
+        raise HTTPException(status_code=400,detail=F"Invalid filed select from {vaild_fields}")
+    
+    if order not in['asc','desc']:
+        raise HTTPException(status_code=400,detail='Invalid order select between asc and desc')
+    
+    data=load_data()
+    sort_order=True if order=="desc" else False
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
