c75c9c5c25608612e23c3286bcc9eff64e4a12e2
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-05-20 11:05:13 +0530

    [Mradul Sharma] Update some validation like(Annotated,Optional and strict)

--- Pydantic/pydantic_understanding.py
+++ Pydantic/pydantic_understanding.py
@@ -1,13 +1,13 @@
 from pydantic import  BaseModel,EmailStr,AnyUrl,Field
-from typing import List,Dict,Annotated
+from typing import List,Dict,Annotated,Optional
 class Patient(BaseModel):
     name:Annotated[str,Field(max_length=50,title='Name of the patien',description="Give name of th patient in less than 50 char",examples=['Nitish','amit'])]
     email:EmailStr
     linkedin_url:AnyUrl
-    age:int
-    weight:float=Field(gt=0,lt=120)
-    married:bool
-    allergies:List[str]
+    age:int=Field(gt=0,lt=120)
+    weight:Annotated[float,Field(gt=0,strict=True)]
+    married:Annotated[bool,Field(default=None,description='Is the patient is married or not')]
+    allergies:Annotated[Optional[List[str]],Field(default=None,max_length=5)]
     contact_details:Dict[str,str]
     
 def insert_patient_data(patient:Patient):
