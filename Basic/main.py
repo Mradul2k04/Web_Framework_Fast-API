42166301b96b2bbd51334f15e1303e0cad0ee6b7
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-05-23 11:57:36 +0530

    [Mradul sharma] Updated the Put endpoint to update my patient details

--- Basic/main.py
+++ Basic/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI , Path,HTTPException,Query
 from fastapi.responses import JSONResponse
 import json
 from pydantic import BaseModel,Field,computed_field
-from typing import Annotated,Literal
+from typing import Annotated,Literal,Optional
 
 app=FastAPI()
 
@@ -32,6 +32,15 @@ class Patient(BaseModel):
             return "Normal"
         else:
             return "Obese"
+        
+class PatientUpdate(BaseModel):
+     name:Annotated[Optional[str],Field(default=None)]
+     city:Annotated[Optional[str],Field(default=None)]
+     age:Annotated[Optional[int],Field(default=None,gt=0)]
+     gender:Annotated[Optional[Literal['male','female','optional']],Field(default=None)]
+     height:Annotated[Optional[float],Field(default=None,gt=0)]
+     weight:Annotated[Optional[float],Field(default=None,gt=0)]
+           
         
 
 def load_data():
@@ -105,7 +114,33 @@ def create_patient(patient:Patient):
     save_data(data)
     
     return JSONResponse(status_code=201,content={"meassage":"patient created succesfully"})
+
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id:str,patient_update:PatientUpdate):
+    data=load_data()
     
+    if patient_id not in data:
+        raise HTTPException(status_code=404,detail="Patient not found")
+    existing_patient_info=data[patient_id]
+     
+    update_patient_info=patient_update.model_dump(exclude_unset=True)
+     
+    for key ,value in update_patient_info.items():
+         existing_patient_info[key]=value
+         
+    existing_patient_info['id']=patient_id
+    patient_pydantic_obj=Patient(**existing_patient_info)
+    existing_patient_info=patient_pydantic_obj.model_dump(exclude='id')
+    
+    data[patient_id]=existing_patient_info
+    
+    save_data(data)
+    return JSONResponse(status_code=200,content={"message":"Patient updated"})
+    
+     
+     
+     
     
     
     
